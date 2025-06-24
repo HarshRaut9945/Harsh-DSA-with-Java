64f5b929ce223386e0ff0761455d37086c2b9f2b
Harsh RAut <harshraut9945@gmail.com>
2025-06-24 23:37:24 +0530

    chsn0

---
+++ BinarySearch/Bsearch.java
@@ -0,0 +1,26 @@
+public class Bsearch {
+
+    static int binarySearch(int arr[],int target){
+        int st=0;
+        int end=arr.length-1;
+        while(st<=end){
+            int mid=(st+end)/2;
+        
+            if(arr[mid]==target){
+                return  mid;
+
+            }
+            if(target<arr[mid]){
+                 end=mid-1;
+            }else{
+               st=mid+1;
+            }
+        }
+  return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]={3,4,6,7,9,10,14,15};
+        int target=15;
+      System.out.print(binarySearch(arr, target));  
+    }
+}
