7692f6c5e5174b46efdf3dcb0ddbc9d8cf626188
Harsh RAut <harshraut9945@gmail.com>
2025-07-12 15:00:34 +0530

    middle

---
+++ LinkedList/INTERVIEWe=ques/Middile_Of_Linkedlist.java
@@ -0,0 +1,45 @@
+public class Middile_Of_Linkedlist {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    // Test the method
+    public static void main(String[] args) {
+        // Creating linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        // head.next.next.next.next.next = new ListNode(6); // Uncomment to test even
+        // case
+
+        ListNode mid = middleNode(head);
+        System.out.println("Middle node value: " + mid.val);
+    }
+}
