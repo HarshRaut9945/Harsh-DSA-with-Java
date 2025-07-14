c5d25f2770bddc4adef4f3a56d61a85562a8433d
Harsh RAut <harshraut9945@gmail.com>
2025-07-14 10:39:58 +0530

    Remove Duplicate

---
+++ LinkedList/INTERVIEWe=ques/Delete_Duplicate.java
@@ -0,0 +1,53 @@
+public class Delete_Duplicate {
+    // Node class for singly linked list
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    
+    public static Node deleteDuplicates(Node head) {
+        Node current = head;
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    
+    public static void main(String[] args) {
+        
+        Node head = new Node(1);
+        head.next = new Node(1);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = new Node(3);
+        head.next.next.next.next.next = new Node(4);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        head = deleteDuplicates(head);
+
+        System.out.println("List after removing duplicates:");
+        printList(head);
+    }
+}
