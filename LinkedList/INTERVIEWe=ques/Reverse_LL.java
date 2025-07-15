8eeeaea392423e416e46c137a95d980dd4a7dc43
Harsh RAut <harshraut9945@gmail.com>
2025-07-15 17:55:44 +0530

    Reverse List

---
+++ LinkedList/INTERVIEWe=ques/Reverse_LL.java
@@ -0,0 +1,51 @@
+public class Reverse_LL {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next); 
+        head.next.next = head;   
+        head.next = null;       
+        return newHead;
+    }
+
+    public static void display(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        display(head);
+
+        ListNode reversed = reverseList(head);
+
+        System.out.println("Reversed List:");
+        display(reversed);
+    }
+}
