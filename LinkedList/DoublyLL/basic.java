e3310c963017bb17637e856c8a2e2f37a75e7fba
Harsh RAut <harshraut9945@gmail.com>
2025-07-19 16:27:59 +0530

    basic connection

---
+++ LinkedList/DoublyLL/basic.java
@@ -0,0 +1,58 @@
+public class basic {
+
+
+  public static class Node{
+       int val;
+       Node next;
+       Node prev;
+
+       Node(int val){
+        this.val=val;
+
+       }
+
+  }
+    public static void  Display(Node head){
+
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.val + " ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Node a=new Node(10);
+        Node b=new Node(20);
+        Node c=new Node(30);
+        Node d=new Node(15);
+        Node e=new Node(60);
+        Node f=new Node(100);
+
+          a.prev = null;
+        a.next = b;
+
+        b.prev = a;
+        b.next = c;
+
+        c.prev = b;
+        c.next = d;
+
+        d.prev = c;
+        d.next = e;
+
+        e.prev = d;
+        e.next = f;
+
+        f.prev = e;
+        f.next = null;
+     
+
+        Display(a);
+        
+    }
+
+    
+}
