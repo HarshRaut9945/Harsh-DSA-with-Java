44e3170b51121f45178bc863abc20dbea267736b
Harsh RAut <harshraut9945@gmail.com>
2025-07-20 17:09:08 +0530

    basic implementation

---
+++ Stackk/Reverse.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+public class Reverse {
+    public static void main(String[] args) {
+        
+        Stack <Integer> st=new Stack <>();
+
+        st.push(1);
+        st.push(3);
+        st.push(4);
+        st.push(6);
+        st.push(7);
+
+        System.out.println(st);
+
+          Stack <Integer> hr=new Stack <>();
+
+          while(st.size()>0){
+            int x=st.peek();
+                hr.push(x);
+                st.pop();
+          }
+
+          System.out.println(hr);
+          
+    }
+    
+}
