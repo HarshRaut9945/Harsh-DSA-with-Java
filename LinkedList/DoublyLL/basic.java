4597d02efc4749d88da7141dcef68671009e73a0
Harsh RAut <harshraut9945@gmail.com>
2025-07-20 15:28:52 +0530

    palindrome for doubly linked list

--- LinkedList/DoublyLL/basic.java
+++ LinkedList/DoublyLL/basic.java
@@ -21,15 +21,35 @@ public class basic {
         }
         System.out.println();
     }
+  
+    
+     public static boolean  palindrme(Node head){
+      Node temp=head;
+
+      while(temp.next!=null){
+        temp=temp.next;
+
+      }
+      Node h=head;
+      Node t=temp;
+
+      while(h!=t){
+        if(h.val!=t.val) return false;
+
+        h=h.next;
+        t=t.prev;
+      }
+      return  true;
+     }
 
     public static void main(String[] args) {
 
         Node a=new Node(10);
         Node b=new Node(20);
         Node c=new Node(30);
-        Node d=new Node(15);
-        Node e=new Node(60);
-        Node f=new Node(100);
+        Node d=new Node(30);
+        Node e=new Node(20);
+        Node f=new Node(10);
 
           a.prev = null;
         a.next = b;
@@ -51,6 +71,14 @@ public class basic {
      
 
         Display(a);
+        
+        if ( palindrme(a)) {
+            System.out.println("The list is a palindrome.");
+        } else {
+            System.out.println("The list is NOT a palindrome.");
+        }
+    
+       
         
     }
 
