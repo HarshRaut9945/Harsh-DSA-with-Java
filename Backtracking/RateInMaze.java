f1c88a048a4c226686c14e31c536390d843fa615
Harsh RAut <harshraut9945@gmail.com>
2025-08-20 18:51:29 +0530

    Rate in Maze

---
+++ Backtracking/RateInMaze.java
@@ -0,0 +1,39 @@
+
+
+public class RateInMaze {
+
+
+    private static  void print(int sr,int sc,int er,int ec,String s,boolean[][] isVisited){
+        if(sr<0 || sc <0) return;
+        if(sr>er || sc>ec) return;
+
+        if(isVisited[sr][sc]==true) return;
+        if(sr==er && sc==ec){
+            System.out.println(s);
+            return;
+        }
+        isVisited[sr][sc]=true;
+
+        print(sr, sc+1, er, ec, s+"R", isVisited);
+        //go down
+        print(sr+1, sc, er, ec, s+"D", isVisited);
+              //go left
+        print(sr, sc-1, er, ec, s+"L", isVisited);
+        //go up
+        print(sr-1, sc, er, ec, s+"U", isVisited);
+        //backtrack
+        isVisited[sr][sc]=false;
+
+
+    }
+
+
+    public static void main(String[] args) {
+        int rows=3;
+        int cols=4;
+        boolean[][] isVisited=new boolean[rows][cols];
+        print(0, 0, rows-1, cols-1, "", isVisited);
+       
+
+    }
+}
