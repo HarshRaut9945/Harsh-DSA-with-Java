ba80065cce7901a53adfb1cc06dfd605b6f79622
Harsh RAut <harshraut9945@gmail.com>
2025-11-09 01:59:42 +0530

    cd

---
+++ Trees/sizeMaxSumHeight.java
@@ -0,0 +1,56 @@
+public class sizeMaxSumHeight {
+
+    public static class Node{
+        int val;
+        Node left;
+        Node right;
+
+        Node(int val){
+            this.val=val;
+        }
+    }
+
+    public static void display(Node root){
+        if(root==null) return;
+        System.out.print(root.val+" ->");
+      if(root.left!=null)  System.out.print(root.left.val+"  ");
+      if(root.right!=null)   System.out.print(root.right.val);
+        System.out.println();
+          display(root.left); 
+        display(root.right);
+    }
+
+      public static void preorder(Node root){
+        if(root==null) return;
+           System.out.println(root.val);
+           preorder(root.left);
+           preorder(root.right);
+
+      }
+
+      public static int size(Node root){
+        if(root==null) return 0;
+
+        return 1 + size(root.left) +  size(root.right);
+      }
+    public static void main(String[] args) {
+
+        Node root=new Node(1);
+        Node a=new Node(2);
+        Node b=new Node(3);
+
+         root.left=a;
+         root.right=b;
+             Node c=new Node(4);
+             Node d=new Node(5);
+             Node e=new Node(6);
+             Node f=new Node(7);
+
+             a.left=c;
+             a.right=d;
+             b.right=e;
+             b.left=f;
+          System.out.println(size(root));
+        
+    }
+}
